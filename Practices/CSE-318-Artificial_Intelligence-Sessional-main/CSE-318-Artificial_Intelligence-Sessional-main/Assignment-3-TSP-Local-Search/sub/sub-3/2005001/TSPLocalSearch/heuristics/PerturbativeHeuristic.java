b6b443bd0e8efe96e560e4a10327426a08b74f75
Apurbo <2105057@cse.buet.ac.bd>
2025-04-27 15:53:26 +0600

    Offline-1 done

--- Practices/CSE-318-Artificial_Intelligence-Sessional-main/CSE-318-Artificial_Intelligence-Sessional-main/Assignment-3-TSP-Local-Search/sub/sub-3/2005001/TSPLocalSearch/heuristics/PerturbativeHeuristic.java
+++
@@ -1,8 +0,0 @@
-package heuristics;
-
-import problem.TSP;
-import problem.TSPSolution;
-
-public interface PerturbativeHeuristic {
-    public TSPSolution perturbSolution(TSPSolution solution, boolean verbose);
-}
--- Practices/CSE-318-Artificial_Intelligence-Sessional-main/CSE-318-Artificial_Intelligence-Sessional-main/Assignment-3-TSP-Local-Search/sub/sub-4/2005001/TSPLocalSearch/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package util;
-
-public class Constants {
-    public static final double FRACTION_THRESHOLD = 1e-10;
-}
